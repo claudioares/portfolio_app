5c527e3e5e925ec0c19be6d588c1fa7857c15fcc
claudioares <claudiojasoares.dev@gmail.com>
2023-12-11 18:12:00 -0300

    :feat commit init

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,15 @@
+export function MyHeader () {
+    return(
+        <>
+            <header className="w-screen bg-black flex justify-center items-center">
+                <img src="" alt="" />
+                <ul className="flex h-[5.9rem] text-grey justify-between w-[20%] items-center">
+                    <li> <a href="">Templates</a></li>
+                    <li> <a href="">Docs</a></li>
+                    <li> <a href="">Blog</a></li>
+                    <li> <a href="">Sobre mim</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,18 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#1a1a1a',
+      'white': '#d9d9d9transparent',
+      'grey': '#cccccc',
+      'green': '#083a33',
+      'green_clean': '#099f8f',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    }
   },
   plugins: [],
 }
