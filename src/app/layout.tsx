966375f5b6998271b4bee8a86bdadca9a53a1d4d
claudioares <claudiojasoares.dev@gmail.com>
2023-12-20 17:13:36 -0300

    :feature add animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter, Montserrat } from 'next/font/google';
-import './globals.css';
 import { MyHeader } from './components/Header/header';
+import './globals.css';
 
 const inter = Inter({ 
   subsets: ['latin']
@@ -26,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${montserrat.className}`} >
+      <body className={`${inter.className} ${montserrat.className}`}>
         <MyHeader />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import {gsap} from "gsap"
 import {ScrollTrigger} from "gsap/ScrollTrigger"
 
 
-
 export default function Home() {
 
   const [typing, setTyping] = useState(false)
